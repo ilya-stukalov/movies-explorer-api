862643bc3e5f82eb28322ef8257ed55b6e81387d
ilya-stukalov <ilya.stukalov@softline.com>
2021-11-06 20:05:21 +0300

    cors fix

--- controllers/users.js
+++ controllers/users.js
@@ -118,6 +118,7 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000,
           httpOnly: true,
           sameSite: 'none',
+          secure: true,
         })
         .status(STATUS_OK)
         .send({ message: 'Авторизация успешна!' });
