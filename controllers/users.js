ba021a44df221338966f1826e4401582f605ce21
ilya-stukalov <ilya.stukalov@softline.com>
2021-11-06 19:50:23 +0300

    cors fix

--- controllers/users.js
+++ controllers/users.js
@@ -117,7 +117,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000,
           httpOnly: true,
-          sameSite: 'strict',
+          sameSite: 'none',
           secure: true,
         })
         .status(STATUS_OK)
