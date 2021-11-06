34b0ed3bf094a18e1db892ce225dcc12fb3c9a89
ilya-stukalov <ilya.stukalov@softline.com>
2021-11-06 16:49:07 +0300

    cors fix

--- utils/constants.js
+++ utils/constants.js
@@ -6,6 +6,7 @@ module.exports.allowedCors = [
   'https://api.movies-explorer-ist.nomoredomains.monster',
   'https://movies-explorer-ist.nomoredomains.xyz',
   'http://localhost:3001',
+  'http://localhost:3000',
 ];
 
 module.exports.BAD_URL = 'не является URL адресом';
