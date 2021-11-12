8c7cff69cc7e09f8f81b1fca5cb49ee43578bbde
ilya-stukalov <ilya.stukalov@softline.com>
2021-11-12 12:30:33 +0300

    Обновлена переменная allowed cors

--- utils/constants.js
+++ utils/constants.js
@@ -3,7 +3,7 @@ module.exports.dbOptions = {};
 module.exports.STATUS_OK = 200;
 
 module.exports.allowedCors = [
-  'https://api.movies-explorer-ist.nomoredomains.monster',
+  'https://api.movies-explorer-ist.nomoredomains.xyz',
   'https://movies-explorer-ist.nomoredomains.xyz',
   'http://localhost:3001',
   'http://localhost:3000',
