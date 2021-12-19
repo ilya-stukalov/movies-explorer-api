a6de04192195b23cc52071d84a885b3e2b52440b
ilya-stukalov <ilya.stukalov@softline.com>
2021-12-19 11:21:04 +0300

    update allowed cors

--- utils/constants.js
+++ utils/constants.js
@@ -3,8 +3,8 @@ module.exports.dbOptions = {};
 module.exports.STATUS_OK = 200;
 
 module.exports.allowedCors = [
-  'https://api.movies-explorer-ist.nomoredomains.xyz',
-  'https://movies-explorer-ist.nomoredomains.xyz',
+  'https://api.movies-explorer.istukalov.ru',
+  'https://movies-explorer.istukalov.ru',
   'http://localhost:3001',
   'http://localhost:3000',
 ];
